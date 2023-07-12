2f437ac2b6a40dcabf62d80910f42e6c4e32af01
Carlos Costoso <carlos.costoso@outlook.com>
2023-07-11 21:12:03 -0400

    Created test files

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    // /**
+    //  * Email is sent to users warning them of impending account deletion one day before deleting
+    //  */
+    // public function test_send_warning_email_to_standard_user_day_before_deletion(): void
+    // {
+
+    // }
+
+    // /**
+    //  * User data is deleted at midnight seven days after it is created
+    //  */
+    // public function test_delete_user_seven_days_after_creation(): void
+    // {
+
+    // }
+
+    /**
+     * Demo account always has a "previous day" log generated at midnight
+     */
+    // public function test_demo_account_always_has_previous_day_log(): void
+    // {
+
+    // }
+}
